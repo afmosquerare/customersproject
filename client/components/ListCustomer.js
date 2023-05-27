e7a496c7321ab4dbce7a3a2af8795e6a78c4314a
Andrés Rengifo <andresm.rengifo@gmail.com>
2023-05-27 08:18:56 -0500

    .

---
+++ client/components/ListCustomer.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { styles } from "../assets/styles/styles";
+
+export default function ListCustomer(){
+    return(
+        <View style={styles.container}>
+            <Text>
+                Aqui se mostraran los clientes
+            </Text>
+        </View>
+    )
+}
